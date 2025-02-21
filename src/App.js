11c6a0222e3c5bc77a98820baa4578883ef4daa7
ShwetaG307 <shwetaghanwat@innovatorstech.com>
2025-02-21 17:30:01 +0530

    modified app file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
-// import MemoExample from "./Components/MemoExample"
-// import CallBackExample from "./Components/UseCallbackExample"
-// import FocusInput from "./Components/FocusInput"
-//  import Counter from "./Components/Counter"
+import MemoExample from "./Components/MemoExample"
+import CallBackExample from "./Components/UseCallbackExample"
+import FocusInput from "./Components/FocusInput"
+ import Counter from "./Components/Counter"
 import Storage from "./Components/Storage"
 function App() {
   return (
   <>
-      {/* <MemoExample/> */}
-      {/* <CallBackExample/> */}
-      {/* <FocusInput/> */}
-      {/* <Counter/> */}
+      <MemoExample/>
+      <CallBackExample/>
+      <FocusInput/>
+      <Counter/>
       <Storage/>  
     </>
   );
